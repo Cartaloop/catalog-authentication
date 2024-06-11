540c433e03de57c0a3fad3c42900482352e060fe
Cartaloop <lucasrech00@gmail.com>
2024-06-11 20:01:21 -0300

    Create product repository

--- src/main/java/edu/lucasrech/catalog_authentication/repository/ProductRepository.java
+++ src/main/java/edu/lucasrech/catalog_authentication/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package edu.lucasrech.catalog_authentication.repository;
 
-public interface ProductRepository {
+import edu.lucasrech.catalog_authentication.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
