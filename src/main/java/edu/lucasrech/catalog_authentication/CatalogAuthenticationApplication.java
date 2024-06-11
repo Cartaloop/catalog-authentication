1b610e4e4873b888473c101006e3159462fcaff6
Cartaloop <lucasrech00@gmail.com>
2024-06-11 18:30:06 -0300

    Initial commit

---
+++ src/main/java/edu/lucasrech/catalog_authentication/CatalogAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package edu.lucasrech.catalog_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lucasrech/catalog_authentication/CatalogAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lucasrech.catalog_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
