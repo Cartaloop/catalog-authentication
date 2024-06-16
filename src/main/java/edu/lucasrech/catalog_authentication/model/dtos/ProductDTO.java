cb42dea78eb01291bf00aad210aa2ba4745703a2
Cartaloop <lucasrech00@gmail.com>
2024-06-16 20:58:21 -0300

    Change dto locale

--- src/main/java/edu/lucasrech/catalog_authentication/model/dtos/ProductDTO.java
+++
@@ -1,6 +0,0 @@
-package edu.lucasrech.catalog_authentication.model.dtos;
-
-import edu.lucasrech.catalog_authentication.model.enums.Category;
-
-public record ProductDTO(String name, String description, double price, String imageUrl, Category category) {
-}
