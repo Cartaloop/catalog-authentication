13c95efa68c51cf5b785cd34df79a90ff499b115
Cartaloop <lucasrech00@gmail.com>
2024-06-16 20:17:11 -0300

    Change the method to increment array string request

--- src/main/java/edu/lucasrech/catalog_authentication/exception/ValueExpectedException.java
+++ src/main/java/edu/lucasrech/catalog_authentication/exception/ValueExpectedException.java
@@ -1,8 +1,14 @@
 package edu.lucasrech.catalog_authentication.exception;
 
 public class ValueExpectedException extends Exception{
-    public ValueExpectedException() {
+    private final String[] request;
+    public ValueExpectedException(String[] request) {
         super("A value is expected. It cannot be null");
+        this.request = request;
+    }
+
+    public String[] getRequest() {
+        return request;
     }
 
 
