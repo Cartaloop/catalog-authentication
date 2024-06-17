a04cedc0050d42f2658fad86e684ef88fb85240a
Cartaloop <lucasrech00@gmail.com>
2024-06-16 21:03:48 -0300

    Implements lombok getter annotation

--- src/main/java/edu/lucasrech/catalog_authentication/exception/ValueExpectedException.java
+++ src/main/java/edu/lucasrech/catalog_authentication/exception/ValueExpectedException.java
@@ -1,14 +1,13 @@
 package edu.lucasrech.catalog_authentication.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ValueExpectedException extends Exception{
     private final String[] request;
     public ValueExpectedException(String[] request) {
         super("A value is expected. It cannot be null");
         this.request = request;
-    }
-
-    public String[] getRequest() {
-        return request;
     }
 
 
