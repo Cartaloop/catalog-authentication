325a178f7224c73cfe9c5e230a68ae1526f2af4b
Cartaloop <lucasrech00@gmail.com>
2024-06-18 20:51:48 -0300

    Create Security Filter Chain

--- src/main/java/edu/lucasrech/catalog_authentication/infra/SecurityConfig.java
+++ src/main/java/edu/lucasrech/catalog_authentication/infra/SecurityConfig.java
@@ -1,4 +1,25 @@
 package edu.lucasrech.catalog_authentication.infra;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+
+        return http.build();
+    }
+
 }
