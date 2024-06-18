daf8163cdcbfa3fff35e6f3d713045171eedf6a3
Cartaloop <lucasrech00@gmail.com>
2024-06-18 20:14:55 -0300

    Create User entity

--- src/main/java/edu/lucasrech/catalog_authentication/model/User.java
+++ src/main/java/edu/lucasrech/catalog_authentication/model/User.java
@@ -1,4 +1,25 @@
 package edu.lucasrech.catalog_authentication.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String username;
+    private String email;
+    private String password;
 }
