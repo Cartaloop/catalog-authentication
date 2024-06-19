ef0a86699e972561e94569c64cc5d0a9369d92d2
Cartaloop <lucasrech00@gmail.com>
2024-06-18 22:35:34 -0300

    Implements UserDetails

--- src/main/java/edu/lucasrech/catalog_authentication/model/UserAuthenticated.java
+++ src/main/java/edu/lucasrech/catalog_authentication/model/UserAuthenticated.java
@@ -1,4 +1,49 @@
 package edu.lucasrech.catalog_authentication.model;
 
-public class UserAuthenticated {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserAuthenticated implements UserDetails {
+    private final User user;
+    public UserAuthenticated(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(user::getRole);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
