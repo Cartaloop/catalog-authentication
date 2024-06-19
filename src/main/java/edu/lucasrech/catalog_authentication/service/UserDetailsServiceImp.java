7a726a514f8551555a4d65c147e3f15cbb12821f
Cartaloop <lucasrech00@gmail.com>
2024-06-18 22:43:42 -0300

    Implements UserDetailService

---
+++ src/main/java/edu/lucasrech/catalog_authentication/service/UserDetailsServiceImp.java
@@ -0,0 +1,21 @@
+package edu.lucasrech.catalog_authentication.service;
+
+import edu.lucasrech.catalog_authentication.model.UserAuthenticated;
+import edu.lucasrech.catalog_authentication.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImp implements UserDetailsService {
+    private final UserRepository userRepository;
+    public UserDetailsServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            return userRepository.findByUsername(username)
+                    .map(UserAuthenticated::new)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
