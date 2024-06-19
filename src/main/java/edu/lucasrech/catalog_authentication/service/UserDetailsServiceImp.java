b81678484cb6b4a592846257af38ce05ec3fac87
Cartaloop <lucasrech00@gmail.com>
2024-06-18 22:54:59 -0300

    add service annotation

--- src/main/java/edu/lucasrech/catalog_authentication/service/UserDetailsServiceImp.java
+++ src/main/java/edu/lucasrech/catalog_authentication/service/UserDetailsServiceImp.java
@@ -5,7 +5,9 @@ import edu.lucasrech.catalog_authentication.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImp implements UserDetailsService {
     private final UserRepository userRepository;
     public UserDetailsServiceImp(UserRepository userRepository) {
